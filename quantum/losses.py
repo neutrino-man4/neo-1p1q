5da0c94c1bfac2de2b7d379bbb80883ee6ab0a72
Aritra <aritrabal2@gmail.com>
2026-09-05 14:47:17 +0200

    Add loss registry, drop sys.exit on unknown loss_type

--- quantum/losses.py
+++ quantum/losses.py
@@ -1,9 +1,16 @@
 import pennylane.numpy as np
-import sys,tqdm
+import tqdm
+from typing import Callable, Dict
 import quantum.math_functions as mfunc
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+
+# name -> scoring function(labels, score); add an entry here to support a new loss_type
+LOSS_FNS: Dict[str, Callable] = {
+    'MSE': mfunc.mean_squared_error,
+    'BCE': mfunc.binary_cross_entropy,
+}
 
 def semi_classical_cost(weights,inputs=None,quantum_circuit=None,return_fid=False):
     if return_fid:
@@ -34,16 +41,10 @@ def batch_semi_classical_cost(weights,inputs=None,quantum_circuit=None,return_fi
 def VQC_cost(weights,inputs=None,quantum_circuit=None,labels=None,return_scores=False,loss_type='MSE',reg=1.):
     bias=weights.aux['bias']
     exp_vals=np.array(quantum_circuit(weights,inputs),requires_grad=True) # n_qubits x batch_size
-    if loss_type=='BCE':
-        score=exp_vals + bias
-        loss_fn=mfunc.binary_cross_entropy(labels,score)
-    elif loss_type=='MSE':
-        score=exp_vals+bias
-        #score=transform(score,k1=k1)
-        #score=mfunc.double_sided_leaky_relu(score)
-        loss_fn=mfunc.mean_squared_error(labels,score)#+reg*score*(1-score)
-    else:
-        sys.exit(-1)
+    score=exp_vals+bias
+    if loss_type not in LOSS_FNS:
+        raise ValueError(f"Unknown loss_type '{loss_type}'. Registered: {list(LOSS_FNS)}")
+    loss_fn=LOSS_FNS[loss_type](labels,score)
     if return_scores:
         return loss_fn, score
     return np.array(loss_fn,requires_grad=True)
@@ -52,24 +53,16 @@ def batched_VQC_cost(weights,inputs=None,quantum_circuit=None,labels=None,return
     bias=weights.aux['bias']
     #k1=weights[-3]
     #k2=weights[-3]
+    if loss_type not in LOSS_FNS:
+        raise ValueError(f"Unknown loss_type '{loss_type}'. Registered: {list(LOSS_FNS)}")
     loss_fn=[]
     scores=[]
     for input,label in tqdm.tqdm(zip(inputs,labels),total=len(inputs)):
         exp_vals=np.array(quantum_circuit(weights,input[None,...]),requires_grad=True) # n_qubits x batch_size
         #exp_vals=np.mean(exp_vals,axis=0)
-    
-        if loss_type=='BCE':
-            score=exp_vals + bias
-            scores.append(score)
-            loss_fn.append(mfunc.binary_cross_entropy(label,score))
-        elif loss_type=='MSE':
-            score=exp_vals+bias
-            #sscore=mfunc.double_sided_leaky_relu(score)
-            #score=transform(score,k1=k1)
-            scores.append(score)
-            loss_fn.append(mfunc.mean_squared_error(label,score))#+reg*score*(1-score))
-        else:
-            sys.exit(-1)
+        score=exp_vals+bias
+        scores.append(score)
+        loss_fn.append(LOSS_FNS[loss_type](label,score))
     loss_fn=np.array(loss_fn,requires_grad=False)
     score=np.array(scores,requires_grad=False)
     if return_scores:
